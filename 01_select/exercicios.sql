d2830575d15b605495918351c96878cf6a94f262
Pedro Lima <pedrolimake19@gmail.com>
2026-09-21 11:01:18 -0300

    Create exercicios.sql for SQL SELECT exercises
    Added exercises for basic SQL SELECT statements.

---
+++ 01_select/exercicios.sql
@@ -0,0 +1,26 @@
+-- SQL Studies
+-- Topic: SELECT
+-- Basic data retrieval
+
+
+-- Exercise 1
+-- Display all products
+SELECT *
+FROM produtos;
+
+
+-- Exercise 2
+-- Display only the product name and price
+SELECT
+    nome,
+    preco
+FROM produtos;
+
+
+-- Exercise 3
+-- Display the product name, category and stock
+SELECT
+    nome,
+    categoria,
+    estoque
+FROM produtos;
