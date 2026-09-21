75ef65187ad96d901889167e118f29a81b4ae76b
Pedro Lima <pedrolimake19@gmail.com>
2026-09-21 11:03:39 -0300

    Add SQL exercises for filtering records

---
+++ 02_where/exercicios.sql
@@ -0,0 +1,31 @@
+-- SQL Studies
+-- Topic: WHERE
+-- Filtering records
+
+
+-- Exercise 1
+-- Display products with a price greater than 500
+SELECT *
+FROM produtos
+WHERE preco > 500;
+
+
+-- Exercise 2
+-- Display products with stock greater than 10
+SELECT *
+FROM produtos
+WHERE estoque > 10;
+
+
+-- Exercise 3
+-- Display products with a price less than 1000
+SELECT *
+FROM produtos
+WHERE preco < 1000;
+
+
+-- Exercise 4
+-- Display products with a price equal to 500
+SELECT *
+FROM produtos
+WHERE preco = 500;
